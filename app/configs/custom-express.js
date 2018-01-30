efd4bdc277bfd320bb313b4e191c9d14f787bffb
Scobin <vhscobin@gmail.com>
2018-01-30 09:31:05 -0200

    Inserção do Express-validator / Alterações na entrada de pagamentos

--- app/configs/custom-express.js
+++ app/configs/custom-express.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 module.exports = function(){
     var app = express();    
     app.use(bodyParser.json());    
+    app.use(expressValidator());
+
     consign({cwd:'app'}).include('controllers').then('models').then('server').into(app);
 
     return app;
--- app/controllers/pagamentos.js
+++ app/controllers/pagamentos.js
@@ -2,20 +2,39 @@ module.exports = function(app){
     app.get('/pagamentos', function(req, res){
         res.send('pagamentos');
     });
-
+    
     app.post('/pagamentos/receber_pagamento', function(req, res){
-        var pagamento = req.body;
         console.log('receber_pagamento: Processando req.');
 
-        pagamento.status = 'CRIADO';
-        pagamento.data = new Date;
+        req.assert("forma_de_pagamento","Forma de pagamento é obrigatório").notEmpty();
+        req.assert("valor","Valor é obrigatório").notEmpty();
+        req.assert("valor","Valor deve ser um decimal").isFloat();
 
+        var erros = req.validationErrors();
+
+        if(erros){
+            console.log('Erro validação');
+            res.status(400).json({"erros":erros});
+            return ;
+        }
+
+        var pagamento = req.body;
         var connection = app.server.connection();
         var pgDAO = new app.models.PagamentoDAO(connection);
 
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+        
         pgDAO.salva(pagamento, function(erro, result){
-            console.log('Salva pagamento');
-            res.json(pagamento);
+            if(erro){
+                res.status(500).json(erro);
+                console.log('erro pag');
+            }
+            else{
+                console.log('pag. criado');
+                res.location('/pagamentos/pagamento/' + result.insertId);
+                res.status(201).json(pagamento);
+            }            
         });
     });
 }
